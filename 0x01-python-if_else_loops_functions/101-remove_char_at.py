adb7f5078572a6a8091798962e2adc3ae39a6e89
Surplusplug <joshuaolagidi@gmail.com>
2023-11-28 20:40:57 -0800

    python: if, else, loops functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-if n &lt; 0:
-return str
-else:
-    str = str[0:n] + str[n+1:]
-return str
-
+    if n &lt; 0:
+        return str
+    else:
+        str = str[0:n] + str[n+1:]
+        return str
