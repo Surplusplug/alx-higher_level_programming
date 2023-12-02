67f191dab2fd59d518781ce158f50866b43163bb
Surplusplug <joshuaolagidi@gmail.com>
2023-12-01 20:27:42 -0800

    python-import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,10 +3,12 @@
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
-    if a &lt; b:
+
+    if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return (c)
-        else:
-            return(sub(a, b))
+        return (c)
+
+    else:
+        return(sub(a, b))
